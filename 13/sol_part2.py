f1de08c5dde8661a9b5d38b6291e779e158a72b4
jakubdrobny <jkubeing@gmail.com>
2022-12-13 10:14:58 +0100

    day 13 both part, bad python code

---
+++ 13/sol_part2.py
@@ -0,0 +1,54 @@
+import ast
+import functools
+
+
+def compare(l1, l2, ind=0):
+    if ind == len(l1) and ind == len(l2):
+        return 0
+    if ind == len(l1):
+        return -1
+    if ind == len(l2):
+        return 1
+
+    if len(l1) and len(l2):
+        if type(l1[ind]) == int and type(l2[ind]) == int:
+            if l1[ind] == l2[ind]:
+                return compare(l1, l2, ind + 1)
+            elif l1[ind] < l2[ind]:
+                return -1
+            else:
+                return 1
+        else:
+            if type(l1[ind]) == int:
+                l1[ind] = [l1[ind]]
+            elif type(l2[ind]) == int:
+                l2[ind] = [l2[ind]]
+            ret = compare(l1[ind], l2[ind], 0)
+            if ret == 0:
+                return compare(l1, l2, ind + 1)
+            return ret
+
+    if len(l1) == 0:
+        return -1
+    if len(l2) == 0:
+        return 1
+    return -1
+
+
+ans = 0
+index = 1
+
+lines = open("test.in", "r").readlines()
+ls = []
+for line_ind in range(0, len(lines), 3):
+    l1 = ast.literal_eval(lines[line_ind].strip())
+    l2 = ast.literal_eval(lines[line_ind + 1].strip())
+    ls.append(l1)
+    ls.append(l2)
+
+ls.append([[2]])
+ls.append([[6]])
+ls.sort(key=functools.cmp_to_key(compare))
+for i in range(len(ls)):
+    print(i + 1, ls[i])
+print(126 * 198)
